3a8e71eb642ab7476f731c3c4ecb73105c20bdeb
sunny2109 <lungsuen@163.com>
2023-04-08 14:29:34 +0800

    Add files via upload

---
+++ NTIRE2023_ESR/models/team14_GFMN.py
@@ -0,0 +1,231 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+# from basicsr.utils.registry import ARCH_REGISTRY
+
+
+# Layer Norm
+class LayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_first"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+        self.normalized_shape = (normalized_shape, )
+
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+
+class GRN(nn.Module):
+    """ GRN (Global Response Normalization) layer
+    """
+    def __init__(self, dim):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.zeros(1, dim, 1, 1))
+        self.beta = nn.Parameter(torch.zeros(1, dim, 1, 1))
+
+    def forward(self, x):
+        Gx = torch.norm(x, p=2, dim=(2, 3), keepdim=True)
+        Nx = Gx / (Gx.mean(dim=1, keepdim=True) + 1e-6)
+        x = self.gamma * (x * Nx) + self.beta + x
+        return x
+
+
+# SE
+class SqueezeExcitation(nn.Module):
+    def __init__(self, dim, shrinkage_rate=0.25):
+        super().__init__()
+        hidden_dim = int(dim * shrinkage_rate)
+
+        self.gate = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(dim, hidden_dim, 1, 1, 0),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, dim, 1, 1, 0),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return x * self.gate(x)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, ffn_expansion_factor, bias=False):
+        super(FeedForward, self).__init__()
+
+        hidden_features = int(dim*ffn_expansion_factor)
+
+        self.project_in = nn.Conv2d(dim, hidden_features*2, kernel_size=1, bias=bias)
+
+        self.dwconv = nn.Conv2d(hidden_features*2, hidden_features*2, kernel_size=3, stride=1, padding=1, groups=hidden_features*2, bias=bias)
+
+        self.project_out = nn.Conv2d(hidden_features, dim, kernel_size=1, bias=bias)
+
+    def forward(self, x):
+        x = self.project_in(x)
+        x1, x2 = self.dwconv(x).chunk(2, dim=1)
+        # x = F.gelu(x1) * x2
+        x = x1 * x2
+        x = self.project_out(x)
+        return x
+
+class ShiftConv2d(nn.Module):
+    def __init__(self, inp_channels, out_channels):
+        super(ShiftConv2d, self).__init__()
+        self.inp_channels = inp_channels
+        self.out_channels = out_channels
+
+        # weight-filter of shape(out_channel,in_channels/groups,kernel_H,kernel_W)
+        self.weight = nn.Parameter(torch.zeros(
+            inp_channels, 1, 3, 3), requires_grad=False)
+        self.n_div = 5
+        g = inp_channels // self.n_div
+        self.weight[0 * g:1 * g, 0, 1, 2] = 1.0  # left
+        self.weight[1 * g:2 * g, 0, 1, 0] = 1.0  # right
+        self.weight[2 * g:3 * g, 0, 2, 1] = 1.0  # up
+        self.weight[3 * g:4 * g, 0, 0, 1] = 1.0  # down
+        self.weight[4 * g:, 0, 1, 1] = 1.0  # identity
+
+        self.conv1x1 = nn.Conv2d(inp_channels, out_channels, 1)
+
+    def forward(self, x):
+        y = F.conv2d(input=x, weight=self.weight, bias=None,
+                     stride=1, padding=1, groups=self.inp_channels)
+        y = self.conv1x1(y)
+        return y
+
+# Spatially-Adaptive Feature Modulation
+class SAFM(nn.Module):
+    def __init__(self, dim, n_levels=4):
+        super().__init__()
+        self.n_levels = n_levels
+        chunk_dim = dim // n_levels
+
+        # Multiscale feature representation
+        self.mfr = nn.ModuleList([nn.Conv2d(chunk_dim, chunk_dim, 3, 1, 1, groups=chunk_dim) for i in range(self.n_levels)])
+
+        # Feature aggregation
+        self.aggr = nn.Conv2d(dim, dim, 1, 1, 0)
+
+        # Activation
+        self.act = nn.GELU() #nn.ReLU() #nn.SiLU(inplace=True) #
+
+    def forward(self, x):
+        h, w = x.size()[-2:]
+
+        xc = x.chunk(self.n_levels, dim=1)
+        out = []
+        for i in range(self.n_levels):
+            if i > 0:
+                p_size = (h//2**(i+1), w//2**(i+1))
+                s = F.adaptive_max_pool2d(xc[i], p_size)
+                s = self.mfr[i](s)
+                s = F.interpolate(s, size=(h, w), mode='nearest')
+            else:
+                s = self.mfr[i](xc[i])
+            out.append(s)
+
+        out = self.aggr(torch.cat(out, dim=1))
+        # Feature modulation
+        out = self.act(out) * x
+        return out
+
+class LKA(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.conv0 = nn.Conv2d(dim, dim, 5, padding=2, groups=dim)
+        self.conv_spatial = nn.Conv2d(dim, dim, 7, stride=1, padding=9, groups=dim, dilation=3)
+        self.conv1 = nn.Conv2d(dim, dim, 1)
+
+
+    def forward(self, x):
+        u = x.clone()
+        attn = self.conv0(x)
+        attn = self.conv_spatial(attn)
+        attn = self.conv1(attn)
+
+        return u * attn
+
+
+class AttBlock(nn.Module):
+    def __init__(self, dim, ffn_scale=2.0):
+        super().__init__()
+
+        self.norm1 = LayerNorm(dim) #GRN(dim)
+        self.norm2 = LayerNorm(dim) #GRN(dim)
+
+        self.safm = SAFM(dim) # #LKA(dim) #SAFM_scale(dim) #
+        self.ffd = FeedForward(dim, ffn_scale)
+
+    def forward(self, x):
+        x = self.safm(self.norm1(x)) + x
+        x = self.ffd(self.norm2(x)) + x
+        return x
+
+
+# @ARCH_REGISTRY.register()
+class GFMN(nn.Module):
+    def __init__(self, dim, n_blocks=8, ffn_scale=2.0, upscaling_factor=4):
+        super().__init__()
+        self.to_feat = nn.Conv2d(3, dim, 3, 1, 1)
+
+        self.feats = nn.Sequential(*[AttBlock(dim, ffn_scale) for _ in range(n_blocks)])
+
+        self.to_img = nn.Sequential(
+            nn.Conv2d(dim, 3 * upscaling_factor**2, 3, 1, 1),
+            nn.PixelShuffle(upscaling_factor)
+        )
+
+    def forward(self, x):
+        x = self.to_feat(x)
+        x = self.feats(x) + x
+        x = self.to_img(x)
+        return x
+
+
+if __name__== '__main__':
+    #############Test Model Complexity #############
+    import time
+    from fvcore.nn import flop_count_table, FlopCountAnalysis, ActivationCountAnalysis
+
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # x = torch.randn(1, 3, 640, 360)
+    # x = torch.randn(1, 3, 427, 240)
+    # x = torch.randn(1, 3, 320, 180)#.to(device)
+    x = torch.randn(1, 3, 256, 256)
+
+    model = GFMN(dim=36, n_blocks=8, ffn_scale=2, upscaling_factor=4)
+    print(model)
+    print(flop_count_table(FlopCountAnalysis(model, x), activations=ActivationCountAnalysis(model, x)))
+    output = model(x)
+    print(output.shape)
+
+    # model.eval()
+    # model.to(device)
+
+    # # Warn-up
+    # for _ in range(5):
+    #     start = time.time()
+    #     outputs = model(x)
+    #     torch.cuda.synchronize()
+    #     end = time.time()
+    #     print('Time:{}ms'.format((end-start)*1000))
+
+    # with torch.autograd.profiler.profile(enabled=True, use_cuda=True, record_shapes=False, profile_memory=False) as prof:
+    #     outputs = model(x)
+    # print(prof.table())
+
+
+
